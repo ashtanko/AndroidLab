044f1038b30e690c99d18043b62dd242e7f5b763
Oleksii Shtanko <shtankopro@gmail.com>
2025-08-03 20:16:55 +0100

    Fix instrumented tests

--- app/src/androidTest/kotlin/dev/shtanko/lab/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/dev/shtanko/lab/app/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("dev.shtanko.androidlab", appContext.packageName)
+        assertEquals("dev.shtanko.lab.app", appContext.packageName)
     }
 }
